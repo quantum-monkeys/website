32c402a14625bdee4079d969742ad5f6107e7e83
Axel Moussard <axel.moussard@gmail.com>
2016-02-29 23:11:37 -0500

    Add contact form

---
+++ contacts.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message']))
+{
+	$to = 'axel.moussard@gmail.com';
+
+	$subject = $_POST['subject'];
+	$message = $_POST['message'] . "\n\n" . 'Regards, ' . $_POST['name'] . '.';
+	$headers = 'From: ' . $_POST['name'] . "\r\n" . 'Reply-To: ' . $_POST['email'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $message, $headers);
+	echo 'success';
+} else {
+	echo 'error';
+}
