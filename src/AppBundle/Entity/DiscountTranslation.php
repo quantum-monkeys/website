1f2544eeeefe705aa3ddf5b81cdac895f8e09040
Axel Moussard <axel.moussard@gmail.com>
2016-11-15 08:48:13 -0500

    Add discounts page

---
+++ src/AppBundle/Entity/DiscountTranslation.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class DiscountTranslation
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * @var Discount
+     */
+    private $discount;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param string $locale
+     */
+    public function setLocale(string $locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * @return Discount
+     */
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    /**
+     * @param Discount $discount
+     */
+    public function setDiscount(Discount $discount)
+    {
+        $this->discount = $discount;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return DiscountTranslation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return DiscountTranslation
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
