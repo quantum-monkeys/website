d7c595a6f3976c7e0d08a4b595f9261f851d669a
Marie Pelletier <marie.pelletier@gmail.com>
2017-01-31 09:50:22 -0500

    fixed metas for blog posts

--- docker/fpm/entrypoint.sh
+++ docker/fpm/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # Change www-data's uid & guid to be the same as directory in host
 # Fixes cache problems
-sed -ie "s/`id -u www-data`/`stat -c %u /var/www/html`/g" /etc/passwd
+# sed -ie "s/`id -u www-data`/`stat -c %u /var/www/html`/g" /etc/passwd
 
-php-fpm
+php-fpm -R
