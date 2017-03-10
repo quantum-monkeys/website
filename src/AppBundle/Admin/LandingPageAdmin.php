efa793361aef7aeac2d5b80592e9983fc1609577
Axel Moussard <axel.moussard@gmail.com>
2017-03-10 12:13:29 -0500

    Create an associated mailchimp list when we create a landing page

--- src/AppBundle/Admin/LandingPageAdmin.php
+++ src/AppBundle/Admin/LandingPageAdmin.php
@@ -2,9 +2,8 @@
 
 namespace AppBundle\Admin;
 
-use AppBundle\Entity\EventOccurrenceCost;
-use AppBundle\Entity\Person;
-use AppBundle\Entity\PersonTranslation;
+use AppBundle\Entity\LandingPage;
+use AppBundle\Manager\MailchimpListManager;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
@@ -12,13 +11,31 @@ use Sonata\AdminBundle\Form\FormMapper;
 
 class LandingPageAdmin extends Admin
 {
+    /**
+     * @var MailchimpListManager
+     */
+    protected $mailchimpListManager;
+
+    /**
+     * @param string $code
+     * @param string $class
+     * @param string $baseControllerName
+     * @param MailchimpListManager $mailchimpListManager
+     */
+    public function __construct($code, $class, $baseControllerName, MailchimpListManager $mailchimpListManager)
+    {
+        parent::__construct($code, $class, $baseControllerName);
+
+        $this->mailchimpListManager = $mailchimpListManager;
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
             ->add('translations', 'sonata_type_collection', [
                 'by_reference' => false,
             ], [
-                'edit' => 'standard',
+                'edit' => 'inline',
                 'inline' => 'table',
                 'admin_code' => 'admin.landing_page_translation',
             ])
@@ -27,7 +44,6 @@ class LandingPageAdmin extends Admin
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-//        $datagridMapper;
     }
 
     protected function configureListFields(ListMapper $listMapper)
@@ -37,14 +53,25 @@ class LandingPageAdmin extends Admin
         ;
     }
 
-
+    /**
+     * @param LandingPage $object
+     */
     public function prePersist($object)
     {
         $this->preUpdate($object);
     }
 
+    /**
+     * @param LandingPage $object
+     */
     public function preUpdate($object)
     {
         $object->setTranslations($object->getTranslations());
+
+        foreach ($object->getTranslations() as $translation) {
+            if ($translation->getMailChimpListId() === null) {
+                $translation->setMailChimpListId($this->mailchimpListManager->create($translation->getTitle(), $translation->getLocale()));
+            }
+        }
     }
 }
